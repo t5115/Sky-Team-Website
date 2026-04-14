0379b7bc2bb29020e77a4bb99e378250fb3a17af
Yanis101K <yaniskaced41@gmail.com>
2026-04-14 14:32:15 +0100

    create Person model and test it with site admin page

--- profilesection/myapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Profile
-# Register your models here.
-admin.site.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ("id", 'name', 'email', 'department', 'role', 'team', 'phone_number')
-    search_fields = ('name', 'email', 'department', 'role', 'team')
