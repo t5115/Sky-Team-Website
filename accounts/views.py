0897b7d39a4f960e085551f3bf907425a9947f14
Shaifur Rahman <rshaifur84@gmail.com>
2026-04-18 01:20:48 +0100

    Dashboard CSS

--- accounts/views.py
+++ accounts/views.py
@@ -49,6 +49,7 @@ def request_registration_link_view(request):
                         "email": email,
                     }
                 )
+                print(registration_link.replace("\n", "").replace("\r", ""))
 
                 email_message = EmailMessage(
                     subject=subject,
